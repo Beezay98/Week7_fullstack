41660584d340413c10fa2c31db38b958657ade40
Beezay98 <sk448766@gmail.com>
2026-01-07 08:47:52 +0545

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "student_portal";
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+    // LOCAL
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "student_portal";
+} else {
+    // SERVER
+    $host = "localhost";
+    $user = "np03cs4a240043";
+    $password = "ZcTaqAnZjf";
+    $database = "np03cs4a240043";
+}
 
 $conn = new mysqli($host, $user, $password, $database);
 
@@ -10,3 +19,4 @@ if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 ?>
+
